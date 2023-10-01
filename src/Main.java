eea5ee0e421daac507f959c96f82d8c6a1c97628
DearVadim <kuzmin-home@yandex.ru>
2023-10-01 21:27:54 +0300

    Сделал PhoneID переменной внутри класса, с сохранением в неё значения статичной переменной PhoneCounter

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,19 @@ public class Main {
     public static void main(String[] args) {
 
         Phone Samsung = new Phone("+79999999999","Samsung Galaxy S20",(short)190);
-        System.out.printf("Модель: %s,\t номер: %s,\t вес: %d г.\n\n", Samsung.getModel(), Samsung.getNumber(), Samsung.getWeight());
+        System.out.printf("Телефон %d. Модель: %s,\t номер: %s,\t вес: %d г.\n\n", Samsung.getPhoneID(), Samsung.getModel(), Samsung.getNumber(), Samsung.getWeight());
 
         Phone Xiaomi = new Phone("+79999999988","Xiaomi Redmi 10A");
         System.out.printf("Модель: %s,\t номер: %s,\t вес: %d г.\n", Xiaomi.getModel(), Xiaomi.getNumber(), Xiaomi.getWeight());
         Xiaomi.setWeight((short) 194);
         System.out.println("Добавили вес. Стало:");
-        System.out.printf("Модель: %s,\t номер: %s,\t вес: %d г.\n\n", Xiaomi.getModel(), Xiaomi.getNumber(), Xiaomi.getWeight());
+        System.out.printf("Телефон %d. Модель: %s,\t номер: %s,\t вес: %d г.\n\n", Xiaomi.getPhoneID(), Xiaomi.getModel(), Xiaomi.getNumber(), Xiaomi.getWeight());
 
         Phone Apple = new Phone();
         Apple.setModel("Apple iPhone 15 Pro");
         Apple.setNumber("+79999999977");
         Apple.setWeight((short) 187);
-        System.out.printf("Модель: %s,\t номер: %s,\t вес: %d г.\n\n", Apple.getModel(), Apple.getNumber(), Apple.getWeight());
+        System.out.printf("Телефон %d. Модель: %s,\t номер: %s,\t вес: %d г.\n\n", Apple.getPhoneID(), Apple.getModel(), Apple.getNumber(), Apple.getWeight());
 
         Xiaomi.receiveCall("Иван Петрович");
         Xiaomi.receiveCall("Семен Павлов");
